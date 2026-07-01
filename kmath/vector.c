82a5c43637b2d97520a17653b583ddaca3e1227a
syntaxDuck <kam.comer96@gmail.com>
2026-07-01 03:32:09 -0500

    Added vector array handeling

--- kmath/vector.c
+++ kmath/vector.c
@@ -2,8 +2,15 @@
 #include <math.h>
 #include <stdio.h>
 
+void km_vec2_print(Vec2 vec) { printf("X: %f, Y: %f\n", vec.x, vec.y); }
+
+void km_vec2_print_detailed(Vec2 vec) {
+    printf("X: %f, Y: %f, Mag: %f, Angle: %f\n", vec.x, vec.y,
+           km_vec2_length(vec), km_vec2_angle(vec));
+}
+
 KmVectorResult km_vec2_set_length(Vec2 *vec, double length) {
-    if (vec == NULL)
+    if (!vec)
         return KM_VEC_RESULT_INVALID_ARG;
 
     double curr_length = km_vec2_length(*vec);
@@ -18,13 +25,8 @@ KmVectorResult km_vec2_set_length(Vec2 *vec, double length) {
     return KM_VEC_RESULT_OK;
 }
 
-void km_vec2_print_detailed(Vec2 vec) {
-    printf("X: %f, Y: %f, Mag: %f, Angle: %f\n", vec.x, vec.y,
-           km_vec2_length(vec), km_vec2_angle(vec));
-}
-
 KmVectorResult km_vec2_normalize(Vec2 *vec) {
-    if (vec == NULL)
+    if (!vec)
         return KM_VEC_RESULT_INVALID_ARG;
 
     double length = km_vec2_length(*vec);
@@ -38,7 +40,7 @@ KmVectorResult km_vec2_normalize(Vec2 *vec) {
 }
 
 KmVectorResult km_vec2_rotate(Vec2 *vec, double angle) {
-    if (vec == NULL)
+    if (!vec)
         return KM_VEC_RESULT_INVALID_ARG;
 
     double newX = vec->x * cos(angle) - vec->y * sin(angle);
@@ -65,4 +67,76 @@ KmVectorResult km_vec2_angle_between(Vec2 vec_a, Vec2 vec_b, double *out) {
 
 double km_vec2_cross(Vec2 vec_a, Vec2 vec_b) {
     return vec_a.x * vec_b.y - vec_a.y * vec_b.x;
+}
+
+static KmVectorResult km_vec_array_create(void **out, size_t element_size,
+                                          size_t cap) {
+    if (!out || cap == 0 || element_size == 0)
+        return KM_VEC_RESULT_INVALID_ARG;
+
+    *out = calloc(cap, element_size);
+    if (!out) {
+        return KM_VEC_RESULT_ALLOC_ERROR;
+    }
+    return KM_VEC_RESULT_OK;
+}
+
+KmVectorResult km_vec2_array_create(Vec2Array *array, size_t cap) {
+    if (!array)
+        return KM_VEC_RESULT_INVALID_ARG;
+
+    void *items = NULL;
+
+    KmVectorResult res = km_vec_array_create(&items, sizeof(Vec2), cap);
+    if (res != KM_VEC_RESULT_OK)
+        return res;
+
+    array->items = items;
+    array->capacity = cap;
+    array->len = 0;
+
+    return KM_VEC_RESULT_OK;
+}
+
+KmVectorResult km_vec2_array_destroy(Vec2Array *array) {
+    if (!array)
+        return KM_VEC_RESULT_INVALID_ARG;
+
+    if (array->items)
+        free(array->items);
+    array->items = NULL;
+    array->capacity = 0;
+    array->len = 0;
+
+    return KM_VEC_RESULT_OK;
+}
+
+KmVectorResult km_vec3_array_create(Vec3Array *array, size_t cap) {
+    if (!array)
+        return KM_VEC_RESULT_INVALID_ARG;
+
+    void *items = NULL;
+
+    KmVectorResult res = km_vec_array_create(&items, sizeof(Vec3), cap);
+    if (res != KM_VEC_RESULT_OK)
+        return res;
+
+    array->items = items;
+    array->capacity = cap;
+    array->len = 0;
+
+    return KM_VEC_RESULT_OK;
+}
+
+KmVectorResult km_vec3_array_destroy(Vec3Array *array) {
+    if (!array)
+        return KM_VEC_RESULT_INVALID_ARG;
+
+    if (array->items)
+        free(array->items);
+    array->items = NULL;
+    array->capacity = 0;
+    array->len = 0;
+
+    return KM_VEC_RESULT_OK;
 }
--- kmath/vector.h
+++ kmath/vector.h
@@ -17,6 +17,7 @@ typedef enum {
     KM_VEC_RESULT_OK,
     KM_VEC_RESULT_INVALID_ARG,
     KM_VEC_RESULT_LENGTH_ZERO,
+    KM_VEC_RESULT_ALLOC_ERROR
 } KmVectorResult;
 
 typedef struct {
@@ -37,6 +38,13 @@ typedef struct {
 
 } Vec2Array;
 
+typedef struct {
+    Vec3 *items;
+    size_t len;
+    size_t capacity;
+
+} Vec3Array;
+
 static inline double km_clamp(double value, double min, double max) {
     if (value < min)
         return min;
@@ -47,20 +55,18 @@ static inline double km_clamp(double value, double min, double max) {
     return value;
 }
 
-static inline Vec2 km_vec2_create(double x, double y) {
-    Vec2 vec = {x, y};
-    return vec;
-}
+static inline Vec2 km_vec2_create(double x, double y) { return (Vec2){x, y}; }
 
 static inline double km_vec2_length(Vec2 vec) {
     return sqrt(vec.x * vec.x + vec.y * vec.y);
 }
 
 static inline KmVectorResult km_vec2_translate(Vec2 *vec, Vec2 offset) {
-    if (vec == NULL)
+    if (!vec)
         return KM_VEC_RESULT_INVALID_ARG;
 
     *vec = km_vec2_create(vec->x + offset.x, vec->y + offset.y);
+    return KM_VEC_RESULT_OK;
 }
 
 static inline double km_vec2_angle(Vec2 vect) {
@@ -72,17 +78,58 @@ static inline double km_vec2_angle(Vec2 vect) {
     return angle;
 }
 
-static inline void km_vec2_print(Vec2 vec) {
-    printf("X: %f, Y: %f\n", vec.x, vec.y);
-}
-
 static inline double km_vec2_dot(Vec2 vec_a, Vec2 vec_b) {
     return vec_a.x * vec_b.x + vec_a.y * vec_b.y;
 }
 
+static inline Vec3 km_vec3_create(double x, double y, double z) {
+    return (Vec3){x, y, z};
+}
+
+static inline double km_vec3_length(Vec3 vec) {
+    return sqrt(vec.x * vec.x + vec.y * vec.y + vec.z * vec.z);
+}
+
+static inline KmVectorResult km_vec3_translate(Vec3 *vec, Vec3 offset) {
+    if (!vec)
+        return KM_VEC_RESULT_INVALID_ARG;
+
+    *vec =
+        km_vec3_create(vec->x + offset.x, vec->y + offset.y, vec->z + offset.z);
+    return KM_VEC_RESULT_OK;
+}
+
+// TODO
+/* static inline double km_vec3_angle(Vec3 vect) { */
+/*     double angle = atan2(vect.y, vect.x); */
+
+/*     if (angle < 0) */
+/*         angle += KM_TAU; */
+
+/*     return angle; */
+/* } */
+
+// TODO
+/* static inline double km_vec3_dot(Vec3 vec_a, Vec3 vec_b) { */
+/*     return vec_a.x * vec_b.x + vec_a.y * vec_b.y; */
+/* } */
+
 KmVectorResult km_vec2_set_length(Vec2 *vec, double length);
 KmVectorResult km_vec2_normalize(Vec2 *vec);
 KmVectorResult km_vec2_rotate(Vec2 *vect, double angle);
+KmVectorResult km_vec2_array_create(Vec2Array *array, size_t cap);
+KmVectorResult km_vec2_array_destroy(Vec2Array *array);
+KmVectorResult km_vec3_array_create(Vec3Array *array, size_t cap);
+KmVectorResult km_vec3_array_destroy(Vec3Array *array);
+
+void km_vec2_print(Vec2 vec);
 void km_vec2_print_detailed(Vec2 vec);
+
+// TODO
+/* void km_vec3_print(Vec3 vec); */
+/* void km_vec3_print_detailed(Vec3 vec); */
+/* KmVectorResult km_vec3_set_length(Vec3 *vec, double length); */
+/* KmVectorResult km_vec3_normalize(Vec3 *vec); */
+/* KmVectorResult km_vec3_rotate(Vec3 *vect, double angle); */
 
 #endif
